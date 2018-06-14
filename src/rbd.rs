9f825c7ab3b95aa10fca43d40f1caee1f9833d9e
Chris Holcombe <xfactor973@gmail.com>
2018-06-14 08:27:43 -0700

    Remove commented Drop impl

--- src/rbd.rs
+++ src/rbd.rs
@@ -12,7 +12,6 @@ use std::collections::HashMap;
 use std::ffi::CString;
 use std::io::{BufRead, Cursor};
 use std::mem::size_of;
-use std::ops::Drop;
 use std::os::raw::{c_char, c_void};
 use std::ptr;
 
@@ -20,23 +19,8 @@ pub struct Rbd;
 
 #[derive(Debug)]
 pub struct RbdImage {
-    pub image: rbd_image_t,
+    image: rbd_image_t,
 }
-
-/*
-impl Drop for RbdImage {
-    fn drop(&mut self) {
-        if !self.image.is_null() {
-            unsafe {
-                let retcode = rbd_close(self.image);
-                if retcode < 0 {
-                    error!("RbdImage Drop failed");
-                }
-            }
-        }
-    }
-}
-*/
 
 pub struct LockOwner {
     pub name: String,
